22d3b6c1841afc52b24b6218f80b05150c1867a5
madotuki <1341syobon@gmail.com>
2016-05-04 23:46:39 +0900

    ページネーションUIを作成
    ページネーションを行うためのUIを作成
    これはページネーションを発火させるためのコンポーネントであり、実際の機能は親コンポーネントに任せる

---
+++ app/assets/javascripts/components/pager.es6.jsx
@@ -0,0 +1,36 @@
+class Pager extends React.Component {
+  constructor (props) {
+    super(props);
+  }
+
+  handleSubmit (e) {
+    e.preventDefault();
+    this.props.onSubmit();
+  }
+
+  render () {
+    if (this.props.hasNextPage) {
+      reutrn (
+        <form className="pager">
+          <button
+            type="submit"
+            className="btn btn-default btn-lg"
+            onClick={this.handleSubmit.bind(this)}
+          >
+            {'次の10件を探す'}
+          </button>
+        </form>
+      );
+    }
+
+    return (
+      <div className="pager">
+      </div>
+    );
+  }
+}
+
+Pager.propTypes = {
+  hasNextPage: React.PropTypes.bool,
+  onSubmit:    React.PropTypes.func
+};
